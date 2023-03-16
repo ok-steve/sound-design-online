4f322dfe5c87d727d30e7dcebfc5b04d8efdc1bc
Steve Cherry <steve@stevecherry.net>
2023-03-16 10:57:56 -0400

    Sync with starter template

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,43 +1,25 @@
-const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
-const pkg = require('./package.json');
+const { EleventyHtmlBasePlugin } = require('@11ty/eleventy');
+const EleventyNavigation = require('@11ty/eleventy-navigation');
 
 module.exports = function (eleventyConfig) {
-  const pathPrefix =
-    process.env.ELEVENTY_RUN_MODE === 'build' ? `/${pkg.name}/` : '/';
-
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+  eleventyConfig.addPlugin(EleventyNavigation);
 
   eleventyConfig.setServerPassthroughCopyBehavior('passthrough');
   eleventyConfig.addPassthroughCopy('./public');
 
-  eleventyConfig.addFilter('parsePrefix', (value, list) =>
-    list.reduce((res, prefix) => (value.includes(prefix) ? prefix : res), '')
+  ['mapPrefix', 'parsePrefix'].forEach((filter) =>
+    eleventyConfig.addFilter(filter, require(`./lib/filters/${filter}`))
   );
 
-  eleventyConfig.addFilter('mapPrefix', (value, prefix) =>
-    value.map((item) => `${prefix}${item}`)
+  ['card', 'field'].forEach((shortcode) =>
+    eleventyConfig.addPairedShortcode(
+      shortcode,
+      require(`./lib/shortcodes/${shortcode}`)
+    )
   );
-
-  eleventyConfig.addPairedShortcode('card', (content, title, id) => {
-    return `
-      <fieldset id="${id}">
-        <legend>${title}</legend>
-        ${content}
-      </fieldset>
-    `;
-  });
-
-  eleventyConfig.addPairedShortcode('field', (content, label, attrs) => {
-    return `
-      <label for="${attrs.id}">${label}</label>
-      ${content}
-    `;
-  });
 
   return {
-    pathPrefix,
-    htmlTemplateEngine: 'njk',
-    markdownTemplateEngine: 'njk',
     dir: {
       input: 'src',
       output: 'dist',
--- src/_data/site.js
+++ src/_data/site.js
@@ -2,14 +2,13 @@ const pkg = require('../../package.json');
 
 module.exports = () => {
   return {
+    env:
+      process.env.ELEVENTY_RUN_MODE === 'build' ? 'production' : 'development',
     name: pkg.name
       .split('-')
       .map(
         (str) => `${str.charAt(0).toUpperCase()}${str.slice(1).toLowerCase()}`
       )
       .join(' '),
-    description: pkg.description,
-    url: `${pkg.author.url}/${pkg.name}`,
-    author: pkg.author,
   };
 };
