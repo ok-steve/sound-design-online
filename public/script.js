df85011c6818129de38f1622144eef42f55955dd
Steve Cherry <2136364+ok-steve@users.noreply.github.com>
2024-01-01 21:48:20 -0500

    Remove form event listener

--- public/script.js
+++ public/script.js
@@ -1,8 +1,3 @@
-// Disable normal form submission
-document.querySelector("form").addEventListener("submit", (e) => {
-  e.preventDefault();
-});
-
 // Sync input and output values
 document.querySelectorAll('input[type="range"][id]').forEach((input) => {
   input.addEventListener("input", (e) => {
